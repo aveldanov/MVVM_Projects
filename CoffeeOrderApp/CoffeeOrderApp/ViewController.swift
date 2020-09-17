1792015e73048ae47aadf5e7175a302612e92155
Anton Veldanov <anton.veldanov@gmail.com>
2020-09-16 20:08:08 -0700

    first commit

---
+++ CoffeeOrderApp/CoffeeOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeOrderApp
+//
+//  Created by Veldanov, Anton on 9/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
