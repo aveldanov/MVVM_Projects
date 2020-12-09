59caf86d224ae4b001c3a15716ecd42899ab86d0
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-08 21:44:32 -0800

    iOS

---
+++ NewsListApp/NewsListApp/Cell/ArticleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsListApp
+//
+//  Created by Veldanov, Anton on 12/8/20.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
