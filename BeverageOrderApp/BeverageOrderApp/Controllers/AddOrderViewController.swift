8e6354bcd132d6c14318f896fdf849e1e23dc2e7
Anton Veldanov <anton_veldanov@intuit.com>
2020-12-20 17:17:34 -0800

    iOS

---
+++ BeverageOrderApp/BeverageOrderApp/Controllers/AddOrderViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddOrderViewController.swift
+//  BeverageOrderApp
+//
+//  Created by Veldanov, Anton on 12/20/20.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
