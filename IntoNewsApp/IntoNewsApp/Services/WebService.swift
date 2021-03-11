739cef09d1454b4b1e1be205c12bef1ced7af8ef
Anton Veldanov <anton.veldanov@gmail.com>
2021-03-10 18:00:55 -0800

    iOS

---
+++ IntoNewsApp/IntoNewsApp/Services/WebService.swift
@@ -0,0 +1,20 @@
+//
+//  WebServices.swift
+//  IntoNewsApp
+//
+//  Created by Veldanov, Anton on 3/10/21.
+//
+
+import Foundation
+
+
+class WebService {
+    func getArticles(url: URL, completion: ([Article])->()){
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error{
+                print(error.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
