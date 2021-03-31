d29377e6e868f3d01f4f655954055926a201fd2d
Anton Veldanov <anton.veldanov@gmail.com>
2021-03-30 20:22:13 -0700

    iOS

--- MVVMUnitTesting/MVVMUnitTesting/Calculator.swift
+++ MVVMUnitTesting/MVVMUnitTesting/Calculator.swift
@@ -11,12 +11,10 @@ class Calculator{
     
     
     func add(_ a:Int, _ b: Int) -> Int{
-        
         return a+b
     }
     
     func subtract(_ a:Int, _ b: Int) -> Int{
-        
         return a-b
     }
 }
