b4656a84fad23a2993856b7fa83d14f2a0c3d052
Anton Veldanov <anton.veldanov@gmail.com>
2021-04-17 00:58:56 -0700

    iOS

---
+++ ItemApp/ItemApp/CategoriesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItemApp
+//
+//  Created by Veldanov, Anton on 4/17/21.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
