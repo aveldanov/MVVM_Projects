1dd23d1a6fa7d343a4b7a7538c5044d8ce803429
Anton Veldanov <anton.veldanov@gmail.com>
2021-04-17 22:47:07 -0700

    iOS

---
+++ PracticeApp/PracticeApp/Controllers/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  PracticeApp
+//
+//  Created by Veldanov, Anton on 4/17/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
